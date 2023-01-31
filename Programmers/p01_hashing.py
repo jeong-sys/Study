ea068673c020e6ac305541df97be2061dd774304
you <20211939@edu.hanbat.ac.kr>
2023-02-01 02:30:23 +0900

    02:30

--- Programmers/p01_hashing.py
+++ Programmers/p01_hashing.py
@@ -1,14 +1,13 @@
 def solution(id_list, report, k):
 
+    answer = []
     # report값 : 신고 한 사람, 신고 받은 사람 동일할 때 삭제해주기
     report_f = set(report)
-    print(report_f)
+    print('1',report_f)
 
     # id_list값 모두 0으로 설정(딕셔너리) => 리스트 딕셔너리 변환
     id_dic = {id:0 for id in id_list}
-    #print(id_dic)
-
-    #test
+    print('2',id_dic)
 
     # report에 배열[1] 값만 불러와서 횟수 세기(딕셔너리 사용해보기)
     ## 리스트 사용해서 분류하여 딕셔너리에 저장하기
@@ -16,7 +15,7 @@ def solution(id_list, report, k):
 
     for called_id in report_f:
         called_list.append((called_id.split())[1])
-    #print(called_list)
+    print('3',called_list)
 
     #초기화된 딕셔너리에 위의 리스트 값 키 겹치면 저장
     for called_id2 in called_list:
@@ -24,56 +23,70 @@ def solution(id_list, report, k):
             id_dic[called_id2] += 1
         else:
             pass
-    #print(id_dic)
+    print('4',id_dic)
 
     # 신고 받은 횟수가 k번 이상일 때, 신고 한 사람한테 메일 보내기
     ## k번 이상 신고 받은 사람 찾기
+    ## *** dictionary로 찾으려고 했는데 안돼서 list로 분류해서 찾음    
 
-    num_list = []
-    for num in id_dic.values():
-        if num >= k:
-            num_list.append(id_dic.keys())
+    user = list(id_dic.keys())
+    num = list(id_dic.values())
 
-    print(num_list)
+    print('0', user, num)
+    idx_list = []
+    idx_f = 0
 
-    ## 신고 받은 사람을 통해서 처음 리스트로 신고 한 사람 찾기
-    # 신고 받은 사람 : 리스트로 만들기
+    ## idx로 사람 정보 append
+    ## 신고 받은 사람 : 리스트로 만들기
+    for n in num:
+        if n >= k:
+            idx_list.append(user[idx_f])
+        idx_f += 1
+    print('00', idx_list)
+
+    ## 신고 받은 사람(idx_list)을 통해서 처음 리스트(report_f)로 신고 한 사람 찾기
     # 처음 리스트 : 분류해서 리스트로 만들기
+    report_list = list(report_f)
+    report_list2 = []
+    for r in report_list:
+        report_list2.append(r.split())
+    
+    print('000',report_list2)
+
     # 신고 받은 사람의 리스트가 처음 리스트에 있을때 처음 리스트 내의 신고 한 사람 뽑기
+    idx_f2 = 0
+    final_call_user = []
+    for find in report_list2:
+        for find2 in idx_list:
+            if find2 == find[1]:
+                final_call_user.append(report_list2[idx_f2])
+        idx_f2 += 1
+    
+    print(final_call_user)  
 
-    answer = []
+    #유저가 받은 메일 수
+    ## 원래 주는 아이디랑 비교해서 딕셔너리 0에서 final_call_user[0] 있으면 하나씩 증가
 
+    print('h', id_list)
+    main_dic = {id:0 for id in id_list}
+    for mail in final_call_user:
+        mail = mail[0]
+        if(main_dic[mail] != None):
+            main_dic[mail] += 1
+        else:
+            pass
+    print('a', main_dic)
+
+    answer = list(main_dic.values())
+    
+    return answer
+     
 # [2, 1, 1, 0]
 print(solution(["muzi", "frodo", "apeach", "neo"],
                ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi", "apeach muzi"], 2))
 # [0, 0]
 print(solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3))
 
-
-# num_police = {
-#     "muzi":0,
-#     "frodo":2,gi
-#     "apeach":0,
-#     "neo":2,
-# }
-#
-# num_mail = {
-#     "muzi": 2,
-#     "frodo": 1,
-#     "apeach": 1,
-#     "neo": 0,
-# }
-
-# ###############==============
-# # 신고 내역
-# history = {}
-# # 신고 횟수 저장
-# report_count = {}
-# # 초기화
-# for x in report:
-#     print(x)
-
-### 디버깅 ~
 '''
 python - pycharm
 c - vscode
---
+++ Programmers/p01_hashing_final.py
@@ -0,0 +1,67 @@
+def solution(id_list, report, k):
+
+    answer = [] # 정답
+
+    '''1회 신고 적용'''
+    report_f = set(report) # report중복 제거
+    
+    id_dic = {id:0 for id in id_list} # id_list 딕셔너리 저장(0으로 초기화)
+    #신고 받은 사람
+    called_list = [] # 신고 받은 사람
+    called_list_k = [] # k번 이상 신고 받은 사람
+    #신고 한 사람
+    call_list = [] # [신고 한 사람, 신고 받은 사람] 
+    call_list_k = [] # k번 이상 [신고 한 사람, 신고 받은 사람]
+    
+    '''신고 받은 횟수 비교'''
+    for called_id in report_f: # 신고 받은 사람만 리스트 생성
+        called_list.append((called_id.split())[1]) 
+
+    for called_id2 in called_list: # 신고 받은 사람 횟수 추가 // 딕셔너리로 표현
+        if(id_dic[called_id2] != None):
+            id_dic[called_id2] += 1
+        else:
+            pass
+
+    '''k번 이상 신고 받은 사람 => 다시 리스트로 변환'''
+    called_id_k = list(id_dic.keys())
+    called_k = list(id_dic.values())
+
+    idx_f = 0 # k번 이상 신고 받은 사람 index
+    for num in called_k:
+        if num >= k:
+            called_list_k.append(called_id_k[idx_f])
+        idx_f += 1
+
+    '''신고 받은 사람(1차원 배열) 기준으로 신고 한 사람(2차원 배열[0]) 찾기'''
+    '''신고 한 사람 찾기 // 리스트 변환'''
+    report_list = list(report_f)
+    for r in report_list:
+        call_list.append(r.split())
+
+    '''k번 이상 신고받은 사람을 신고한 사람 // 리스트 변환'''
+    idx_f2 = 0
+    for call in call_list:
+        for call2 in called_list_k:
+            if call2 == call[1]:
+                call_list_k.append(call_list[idx_f2])
+        idx_f2 += 1
+    
+    '''k번 이상 신고 받은 사람만 리스트 생성 = call_list_k
+        신고 한 사람이 받는 메일 수'''
+    mail_dic = {id:0 for id in id_list} # id_list 딕셔너리로 0초기화
+    print(call_list_k)
+    for mail in call_list_k: # 신고 한 사람 겹치면 1추가
+        mail = mail[0]
+        if(mail_dic[mail] != None):
+            mail_dic[mail] += 1
+        else:
+            pass
+    
+    '''답 출력'''
+    answer = list(mail_dic.values())
+    return answer
+     
+print(solution(["muzi", "frodo", "apeach", "neo"],
+               ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi", "apeach muzi"], 2))
+print(solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3))
