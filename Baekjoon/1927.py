932458e79c7b745879ed3913cc680acb722415ae
jeong-sys <20211939@edu.hanbat.ac.kr>
2023-03-09 15:13:20 +0900

    15:41

---
+++ Baekjoon/1927.py
@@ -0,0 +1,20 @@
+# 최소 힙
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+heap = []
+
+for i in range(n):
+    x = int(sys.stdin.readline())
+
+    # x가 0인경우 가장 작은 값 출력
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap))
+        # 배열이 비어 있으면 0 출력
+        else:
+            print(0)
+    # x 가 0이 아닌경우
+    else:
+        heapq.heappush(heap, x)
